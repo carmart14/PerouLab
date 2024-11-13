cf1a531b56d4e8154af942c6dadbc756e14f665c
carmart14 <139304795+carmart14@users.noreply.github.com>
2024-11-12 22:24:39 -0500

    Beginning Tutorial

---
+++ Seruat5.R
@@ -0,0 +1,15 @@
+# Enter commands in R (or R studio, if installed)
+install.packages('Seurat')
+library(Seurat)
+
+setRepositories(ind = 1:3, addURLs = c('https://satijalab.r-universe.dev', 'https://bnprks.r-universe.dev/'))
+install.packages(c("BPCells", "presto", "glmGamPoi"))
+
+# Install the remotes package
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+install.packages('Signac')
+remotes::install_github("satijalab/seurat-data", quiet = TRUE)
+remotes::install_github("satijalab/azimuth", quiet = TRUE)
+remotes::install_github("satijalab/seurat-wrappers", quiet = TRUE)
