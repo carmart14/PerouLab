ab90f4fc66bd09d35f1bb66ee930b57cdf1b42b4
carmart14 <139304795+carmart14@users.noreply.github.com>
2024-11-13 00:08:45 -0500

    install working and beginning of tutorial

--- Seruat5.R
+++ Seruat5.R
@@ -4,7 +4,6 @@ library(Seurat)
 
 setRepositories(ind = 1:3, addURLs = c('https://satijalab.r-universe.dev', 'https://bnprks.r-universe.dev/'))
 install.packages(c("BPCells", "presto", "glmGamPoi"))
-
 # Install the remotes package
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
@@ -13,3 +12,35 @@ install.packages('Signac')
 remotes::install_github("satijalab/seurat-data", quiet = TRUE)
 remotes::install_github("satijalab/azimuth", quiet = TRUE)
 remotes::install_github("satijalab/seurat-wrappers", quiet = TRUE)
+
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "/Users/ivory/Downloads/GitHub/PerouLab/filtered_gene_bc_matrices/hg19/")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+pbmc
+
+# Quality Control 
+
+# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+
+# Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+
+# FeatureScatter is typically used to visualize feature-feature relationships, but can be used
+# for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
+
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+
+
+# stopping here to better understand the data
